38a491ecb9b83e70ee58583dbf45d7552171ff67
Kenula2003 <75131621+Kenula2003@users.noreply.github.com>
2025-01-06 17:52:13 +0100

    Add files via upload

---
+++ es1.js
@@ -0,0 +1,15 @@
+const rectangleBase = 10; // Valore del variabile
+const RectangleHeight = 20; // Valore del variabile
+const rectangleArea = rectangleBase * RectangleHeight
+//  Somma di Due Variabile
+
+let radiosOfCircle = 10;
+
+
+let priceOfOrange = 10;
+let priceOfGrapes = 12;
+let priceOfApple = 13;
+
+
+let sommaDiDueNumeri = (2 + 5) * 2
+console.log(sommaDiDueNumeri)
